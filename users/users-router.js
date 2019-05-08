7f475bb0bf33bef7e0f6bf21ebff2860a50dddc8
guillsav <guillsav@gmail.com>
2019-05-08 17:45:57 -0400

    Get users returning list of users without password

--- users/users-router.js
+++ users/users-router.js
@@ -7,7 +7,12 @@ router.get('/', restricted, checAccess('Finance'), async (req, res) => {
   try {
     const users = await db.getUsers();
 
-    res.status(200).json(users);
+    res.status(200).json(
+      users.map(({id, username, department}) => {
+        const usersList = {id, username, department};
+        return usersList;
+      })
+    );
   } catch (e) {
     res.status(500).json({
       errorMessage: `Server error couldn't retrieve the users from the database.`
